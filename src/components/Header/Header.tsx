2b8af0ba20f9879d0fea00ec34ac159bb8185615
jmoreira7 <jonas.andrade7@gmail.com>
2025-04-20 16:37:33 +0200

    Setup shadcn/ui and create header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,33 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+  NavigationMenuLink,
+} from "@/components/ui/navigation-menu";
+
+export function Header() {
+  return (
+    <header className="fixed top-0 left-0 w-screen bg-black/50 py-3 flex justify-center">
+      <NavigationMenu>
+        <NavigationMenuList className="flex gap-6">
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              href="/"
+              className="text-gray-200 font-bold"
+            >
+              Home
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              href="#"
+              className="text-gray-200 font-bold"
+            >
+              Placeholder
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </header>
+  );
+}
