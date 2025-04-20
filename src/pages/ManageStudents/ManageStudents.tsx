95a582697bb601474259027099e1af7bb88a710a
jmoreira7 <jonas.andrade7@gmail.com>
2025-04-20 20:40:59 +0200

    Create add student button

--- src/pages/ManageStudents/ManageStudents.tsx
+++ src/pages/ManageStudents/ManageStudents.tsx
@@ -1,4 +1,5 @@
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 import { useState, useEffect } from "react";
 import { api } from "@/utils/api";
 import { Student } from "@/utils/types";
@@ -9,22 +10,32 @@ export function ManageStudents() {
   useEffect(() => {
     console.log("Fetching students...");
 
-    api.get('/students')
-      .then(response => {
+    api
+      .get("/students")
+      .then((response) => {
         console.log("Students fetched successfully:", response.data);
 
         setStudents(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error fetching students:", error);
       });
   }, []);
 
   return (
     <main className="flex flex-col items-center min-h-screen px-4">
-      <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-80">
+      <div className="w-full max-w-3xl flex justify-center mt-24">
+        <Button className="bg-black text-white hover:bg-primary/90">
+          Add Student
+        </Button>
+      </div>
+
+      <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-16">
         {students.map((student, index) => (
-          <Card key={index} className="bg-black/50 border border-gray-300 shadow-md">
+          <Card
+            key={index}
+            className="w-50 h-70 bg-black/50 border border-gray-300 shadow-md"
+          >
             <CardHeader>
               <CardTitle className="text-lg font-bold text-white">
                 {student.name}
