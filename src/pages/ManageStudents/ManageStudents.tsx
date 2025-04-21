4bec4af23c261a08329d2627d133d7a8dbfa946d
jmoreira7 <jonas.andrade7@gmail.com>
2025-04-21 12:30:29 +0200

    Update cards grid columns

--- src/pages/ManageStudents/ManageStudents.tsx
+++ src/pages/ManageStudents/ManageStudents.tsx
@@ -286,7 +286,7 @@ export function ManageStudents() {
         </Dialog>
       )}
 
-      <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-16">
+      <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4 mt-16">
         {students.map((student) => (
           <Card
             key={student._id}
